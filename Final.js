42aa76588e389689ac4d5b4d8480f80db6d1d63e
ShirleyL3 <144644338+ShirleyL3@users.noreply.github.com>
2023-12-03 01:18:27 -0500

    Home Page Created

--- Final.js
+++ Final.js
@@ -1,7 +1,9 @@
-let texting = "Bin Boss";
+let gamename = "Bin";
+let gamename2 = "Boss"
 
 
 function preload(){
+    backphoto = loadImage('data/Oscar.jpeg');
     bin1 = loadImage('data/bin1.png');
     bin2 = loadImage('data/bin2.png');
     bin3 = loadImage('data/bin3.png');
@@ -12,6 +14,7 @@ function preload(){
     font1 = loadFont('data/font1.otf');
     font2 = loadFont('data/font2.ttf');
     font3 = loadFont('data/font3.otf');
+    startp = true;
 }
 
 function setup(){
@@ -23,8 +26,7 @@ function setup(){
     image(trash1, 300, 300);
     trash2.resize(120,0); 
     image(trash2, 400,400);
-
-    let startbutton = new PlayButton(500,500,'Start')
+    startbutton = new Button(160,420,'Start')
 
     // image(bin3, 400,100);
     // image(trash1, random(600), random(600));
@@ -35,15 +37,71 @@ function setup(){
 function draw(){
     // image(trash1, 300, 300);
     // image(trash2, 400,400);
-    fill('#6A9956');
-    textFont(font1,100);
-    text(texting, 100, 100);
+    // fill('#6A9956');
+    // textFont(font1,100);
+    // text(texting, 100, 100);
 
-    textFont(font2,100);
-    text(texting, 100,300);
+    // textFont(font2,100);
+    // text(texting, 100,300);
+
+    if (startp){
+        startPage();
+    }
 
 }
 
-function startPage(){
-    startbutton.diplsay();
+function startPage(){ // displays the homescreen
+    background(0);
+    backphoto.resize(1100,0); //Oscar the grouch photo
+    image(backphoto,-150,100); // loads Oscar photo
+
+    fill('#314123'); //dark green 
+    textFont(font1, 150); 
+    text(gamename, 20, 250); // Displays Game Name
+    text(gamename2, 20, 380);
+
+    startbutton.diplsay(); // displays start button 
+
+    if (startbutton.inButton(mouseX, mouseY)){ // Chnages the button color if the mosue is hovering over
+        startbutton.changecol(255); // white
+    }else{
+        startbutton.changecol('#314123'); // back to green 
+    }
+}
+
+function mousePressed(){
+
+}
+
+
+class Button{ // makes a button
+
+    constructor(x, y, t) { // x position, y position, text
+        this.x = x;
+        this.y = y;
+        this.t = t;
+        this.colorshade = '#00A986';
+    }
+
+    diplsay(){ // displays button in rectangular box and intakes a text input
+        fill(255, 100);
+        rect(this.x -100, this.y - 20, 200, 40, 20);
+        fill(this.colorshade);
+        textFont(font1, 30);
+        text(this.t, this.x-44, this.y + 10);
+    }
+
+    changecol(col){ // method that can change color of button
+        this.colorshade = col;
+    }
+
+    inButton(x2, y2){ // checks if mouse is inside the button
+        var d = dist(x2, y2, this.x, this.y);
+        if (d<40){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
 }
