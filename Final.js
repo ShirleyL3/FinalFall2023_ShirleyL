a4c5e768deb507a18f55d0933af14d9b522187ec
ShirleyL3 <144644338+ShirleyL3@users.noreply.github.com>
2023-12-11 02:57:02 -0500

    Added Photo Monatge

--- Final.js
+++ Final.js
@@ -8,6 +8,8 @@ let screen = 0;
 let trash = new Array(13);
 let trashlst = new Array(13);
 let myfonts = new Array(4);
+let pollutionImg = new Array(5);
+let rescueImg = new Array(5);
 let startp = true;
 let gamep = false;
 let tx;
@@ -18,18 +20,26 @@ let bin2;
 let trashcount = 13;
 
 function preload(){
-    backphoto = loadImage('data/Oscar2.jpeg');
+    backphoto = loadImage('data/Oscar.jpeg');
     bin1 = loadImage('data/bin1.png');
     bin2 = loadImage('data/bin2.png');
-    bin3 = loadImage('data/bin3.png');
 
     for (let i = 0; i< trash.length; i++){ //loads all 13 of my trash pngs
         trash[i] = loadImage('data/trash' + i + '.png');
     }
 
     for (let f = 1; f< myfonts.length; f ++){ //loads my 3 fonts
-        myfonts[f] = loadFont('data/font' + f  + '.otf')
+        myfonts[f] = loadFont('data/font' + f  + '.otf');
     }
+
+    for (let p = 0; p< pollutionImg.length; p ++){ //loads my 5 polltuion jpegs
+        pollutionImg[p] = loadImage('data/pollution' + p  + '.jpeg');
+    }
+
+    for (let r = 0; r< rescueImg.length; r ++){ //loads my 5 rescue Images
+        rescueImg[r] = loadImage('data/rescue' + r  + '.jpeg')
+    }
+
 }
 
 function setup(){
@@ -38,9 +48,7 @@ function setup(){
 
     for(let g = 0; g< trash.length; g++){ // iterates through trash array
         trashlst[g] = new Garbage(trash[g], random(100,700), random(50,430), g);
-        //trashlst.push(new Garbage(trash[g], random(100,700), random(50,430), g));
     }
-
     setInterval(timeCount, 1000);
 }
 
@@ -49,10 +57,82 @@ function draw(){
         startPage();
     }
     if (gamep){
+        console.log(frameCount)
         gameScreen();
+        
         for (let i = 0; i< trashlst.length; i++){
-            trashlst[i].draw();
+            trashlst[i].draw(); // displays trash
+        } 
+        
+        
+        if (trashcount > 0 && countdown == 0){ //if you lose
+            // tried to reset to zero but framecount doesnt continously incriment 
+            frameRate(20);
+            if (frameCount < 100){
+                image(pollutionImg[0],0,0);
+                endText(1);
+            } 
+            else if (frameCount < 200){
+                background(0);
+                image(pollutionImg[1], 0, 0, 0, 800);
+                endText(1);
+            } 
+            else if (frameCount < 300){
+                image(pollutionImg[2], 0, 0);
+                endText(1);
+            }
+            else if (frameCount < 400){
+                image(pollutionImg[3], 0, 0, 1000, 800);
+                endText(1);
+            }
+            else if (frameCount < 500){
+                image(pollutionImg[4], 0, 0);
+                endText(1);
+            }
+            if (frameCount > 500) frameCount = 0;
+            console.log(frameCount);
         }
+        if (trashcount== 0 && countdown >= 0){
+            frameRate(20);
+            if (frameCount < 100){
+                image(rescueImg[0],0,0,800,800);
+                endText(2);
+            } 
+            else if (frameCount < 200){
+                image(rescueImg[1], 0, 0);
+                endText(2);
+            } 
+            else if (frameCount < 300){
+                image(rescueImg[2], 0, 0);
+                endText(2);
+            }
+            else if (frameCount < 400){
+                image(rescueImg[3], 0, 0);
+                endText(2);
+            }
+            else if (frameCount < 500){
+                image(rescueImg[4], 0, 0);
+                endText(2);
+            }
+            if (frameCount > 500) frameCount = 0;
+            console.log(frameCount);
+        }
+        // if state.
+    } // gamep if state. 
+}// end of draw
+
+function endText(num){ // Displays ending text
+    if (num ==1){
+        fill('#D11A0F'); // Red
+        textFont(myfonts[1], 70);
+        text('YOU LOSE!!', 250,400);
+        text('THE WORLD IS DOOMED', 20,470);
+    }
+    if (num == 2){
+        fill('#2ACD22'); // Green
+        textFont(myfonts[1], 70);
+        text('YOU WIN!!', 250,400);
+        text('THE WORLD IS SAVED!', 40,450);
     }
 }
 
@@ -109,18 +189,12 @@ function gameScreen(){
     background(255);
     fill('#314123');
     textFont(myfonts[3],40);
-    text('Trash Left: ' + trashcount, 30,100);
+    text('Trash Left: ' + trashcount, 30,100); // keeps track of trash left 
 
     //Timer 
     if (countdown <= 20) {
         text("Time: " + countdown, 30 , 50);
     }
-    if (countdown == 0) {
-        /// New fucntion
-    }
-
-    // score keeper
-    //https://editor.p5js.org/ehersh/sketches/Hk52gNXR7//
 
     image(bin1, 100, 450);
     image(bin2, 500, 487);
@@ -132,8 +206,82 @@ function timeCount() { // Starts coutdown once on Game Page
     }
 }
 
-//function for tracking number of trash left 
+class Garbage{
 
+    constructor(i,x,y,type){ //image, position x, position y
+        this.i = i;
+        this.x = x;
+        this.y = y;
+        this.type = type
+        this.size = random(90,120);
+        this.overTrash = false;
+        this.picked = false;
+        this.xOffset = 0.0; 
+        this.yOffset = 0.0;
+    }
+
+    draw(){// checks if mouse is inside the trash images
+        if (mouseX > this.x && mouseX < this.x+this.size && 
+            mouseY > this.y && mouseY < this.y+this.size) { // checks if mouse is in image
+            this.overTrash = true;
+        } else {
+            this.overTrash = false;
+        }
+        image(this.i, this.x, this.y, this.size, this.size); // puts the image in a random location while giving it a random size 
+    }
+
+    mousePressed() {
+        if(this.overTrash) { 
+            this.picked = true;
+
+        } else {
+          this.picked = false;
+         
+        }
+        this.xOffset = mouseX-this.x; // movement of each unique object
+        this.yOffset = mouseY-this.y; 
+    }
+
+    mouseDragged() {
+        if(this.picked) {
+          this.x = mouseX-this.xOffset; // new unique position
+          this.y = mouseY-this.yOffset; 
+        }
+    }
+      
+    mouseReleased() {
+        this.picked = false; //drops unique object
+        this.inbin(); 
+    }
+
+    inbin(){ // checks if the trash item is in either of the bins 
+
+        if (this.x > 100 && this.x < 300 && 
+            this.y > 450 && this.y < 550){ // checks if the image is in the area of the garbage bin
+                if (this.type == 1 || this.type == 2 || this.type ==3 || this.type == 5
+                    || this.type ==6 || this.type ==7 || this.type ==8 || this.type == 11 || this.type == 12 ){ // these specific items belong in bin1
+                    for (let i = 0; i < trashlst.length; i++){ 
+                        if(trashlst[i].type == this.type){ // if the type in the list matches the types in the if loop
+                            trashlst.splice(i, 1); // deletes i and takes in index and # of items to del
+                            trashcount -= 1; // deincrements the count by 1 if del
+                        } // if stat
+                    } //for loop
+                } //if stat
+        } //if stat
+
+        if (this.x>500 && this.x < 600 &&
+            this.y >437 && this.y < 487){
+                if (this.type == 0 || this.type ==4 || this.type == 9 || this.type == 10){ // these items belong to bin2
+                    for (let i = 0; i < trashlst.length; i++){
+                        if(trashlst[i].type == this.type){
+                            trashlst.splice(i, 1);
+                            trashcount -=1;
+                        } // if stat
+                    } // for loop
+                }// if stat
+        } //if stat
+    } // inBin Class
+}// class 
 
 class Button{ // makes a button
 
@@ -165,81 +313,3 @@ class Button{ // makes a button
         }
     }
 }
-
-class Garbage{
-
-    constructor(i,x,y,type){ //image, position x, position y
-        this.i = i;
-        this.x = x;
-        this.y = y;
-        this.type = type
-        this.size = random(90,120);
-        this.overTrash = false;
-        this.picked = false;
-        this.xOffset = 0.0; 
-        this.yOffset = 0.0; 
-
-    }
-
-    draw(){// checks if mouse is inside the trash images
-        if (mouseX > this.x && mouseX < this.x+this.size && 
-            mouseY > this.y && mouseY < this.y+this.size) { // checks if mouse is in image
-            this.overTrash = true;
-        } else {
-            this.overTrash = false;
-        } // chnaged tx and ty
-        image(this.i, this.x, this.y, this.size, this.size); // puts the image in a random location while giving it a random size 
-    }
-
-    mousePressed() {
-        if(this.overTrash) { 
-            this.picked = true;
-
-        } else {
-          this.picked = false;
-         
-        }
-        this.xOffset = mouseX-this.x; // movement of each unique object
-        this.yOffset = mouseY-this.y; 
-    }
-
-    mouseDragged() {
-        if(this.picked) {
-          this.x = mouseX-this.xOffset; // new unique position
-          this.y = mouseY-this.yOffset; 
-        }
-    }
-      
-    mouseReleased() {
-        this.picked = false; //drops unique object
-        this.inbin();
-    }
-
-    inbin(){
-
-        if (this.x > 100 && this.x < 300 && 
-            this.y > 450 && this.y < 550){ // 
-                if (this.type == 1 || this.type == 2 || this.type ==3 || this.type == 5
-                    || this.type ==6 || this.type ==7 || this.type ==8 || this.type == 11 || this.type == 12 ){
-                    for (let i = 0; i < trashlst.length; i++){
-                        if(trashlst[i].type == this.type){
-                            trashlst.splice(i, 1);
-                            trashcount -= 1;
-                        }
-                    }
-                } //if stat
-        } //if stat
-
-        if (this.x>500 && this.x < 600 &&
-            this.y >437 && this.y < 487){
-                if (this.type == 0 || this.type ==4 || this.type == 9 || this.type == 10){
-                    for (let i = 0; i < trashlst.length; i++){
-                        if(trashlst[i].type == this.type){
-                            trashlst.splice(i, 1);
-                            trashcount -=1;
-                        } // if stat
-                    } // for loop
-                }// if stat
-        } //if stat
-    } // inBin Class
-}// class 
